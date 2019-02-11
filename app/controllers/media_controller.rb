18e4e90a21140dea709b40eabf22d6ec920ee84a
JoeyHere <josephodell@outlook.com>
2019-02-11 12:13:54 +0000

    Tag boxes

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -9,10 +9,23 @@ class MediaController < ApplicationController
   def show
   end
 
+  def new
+    @medium = Medium.new
+  end
+
+  def create
+    new_medium = Medium.new(medium_params)
+    redirect_to new_medium
+  end
+
   private
 
   def set_medium
     @medium = Medium.find(params[:id])
+  end
+
+  def medium_params
+    params.require(:medium).permit(:title, :type_id)
   end
 
 end
