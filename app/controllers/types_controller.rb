5a62e25d4d24bff279406700135ffd50f2c1ef38
JoeyHere <josephodell@outlook.com>
2019-02-11 10:23:06 +0000

    1

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,13 @@
 class TypesController < ApplicationController
+
+    before_action :set_type, only: [:show]
+
+    def show
+    end
+
+    private
+
+    def set_type
+      @type = Type.find(params[:id])
+    end
 end
