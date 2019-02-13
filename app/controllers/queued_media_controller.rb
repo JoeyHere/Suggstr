bd40666841f67d8232f54ed9dfdd85da031a562b
JoeyHere <josephodell@outlook.com>
2019-02-13 17:27:54 +0000

    suggestions are working

--- app/controllers/queued_media_controller.rb
+++ app/controllers/queued_media_controller.rb
@@ -3,6 +3,22 @@ class QueuedMediaController < ApplicationController
   before_action :find_queued_medium, only: [:destroy]
   before_action :find_user, only: [:destroy]
 
+  def create
+    if !QueuedMedium.find_by(user_id: current_user.id, medium_id: params[:queued_medium][:medium_id])
+      @queued_medium = QueuedMedium.new(user_id: current_user.id)
+      @queued_medium.update(queued_medium_params)
+      flash[:message] = "#{@queued_medium.medium.title} has been added to your que."
+    else
+      flash[:message] = "This is already in your que."
+    end
+
+    current_user.received_suggestions.each do |sug|
+      sug.destroy if sug && sug.medium_id == params[:queued_medium][:medium_id].to_i
+    end
+
+    redirect_to dashboard_path
+  end
+
   def destroy
     @queued_medium.destroy
     redirect_to dashboard_path
@@ -16,6 +32,10 @@ class QueuedMediaController < ApplicationController
 
   def find_user
       @user = current_user
+  end
+
+  def queued_medium_params
+    params.require(:queued_medium).permit(:medium_id)
   end
 
 end
