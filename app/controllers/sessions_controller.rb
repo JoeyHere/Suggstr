b5cfee2fc80b2706c4da22b091034c6e6f21fb18
YSongYS <songyishu.yang@gmail.com>
2019-02-14 17:20:46 +0000

    show page finalized but still need to build the link to the add to queue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
     user = User.find_by(email: params[:session][:email])
      if user && user.authenticate(params[:session][:password])
       # log_in method in app/helpers/session_helper/rb
